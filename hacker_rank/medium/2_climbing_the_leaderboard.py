84dc99b3d2b6d15d79657accf6588e21f5fbc8ae
farhan-shakib <farhancse16.ruet@gmail.com>
2020-10-25 06:42:28 +0600

    comment added to leaderboard climbing

--- hacker_rank/medium/2_climbing_the_leaderboard.py
+++ hacker_rank/medium/2_climbing_the_leaderboard.py
@@ -9,15 +9,18 @@ a_alice = []
 
 last_pos = -1
 
+# get leaderboard 
 def input_leaderboard():
     global n_leaderboard
     global a_leaderboard
     global a_leaderboard_clean
     global last_pos
 
+    # leaderboard list
     n_leaderboard = int(input())
     a_leaderboard[0:n_leaderboard] = input().split()
 
+    # remove dups
     prev = ''
     for rank in a_leaderboard:
         if rank == prev:
@@ -27,8 +30,10 @@ def input_leaderboard():
         rank = int(rank)
         a_leaderboard_clean.append(rank)
 
+    # set last position to botom
     last_pos = len(a_leaderboard_clean)-1
 
+# get player points
 def input_alice():
     global n_alice
     global a_alice
@@ -36,10 +41,15 @@ def input_alice():
     n_alice = int(input())
     a_alice[0:n_alice] = map(int, input().split())
 
+# climb!!!
 def get_rank(alice_score):
     global a_leaderboard_clean
     global last_pos
     
+    # rank = index + 1
+    # if less than current value, then index + 2
+    # if equal, then index + 1
+    # else, if reached top, rank = 1, else, continue climbing
     for i in range(last_pos, -1, -1):
         last_pos = i
         if alice_score < a_leaderboard_clean[i]:
