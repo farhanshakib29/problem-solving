88e3cb00abc35fa360cf20502b5f5f5f1076dae8
farhan-shakib <farhancse16.ruet@gmail.com>
2020-11-24 02:22:46 +0600

    solved bigger is greater

---
+++ hacker_rank/medium/8_bigger_is_greater.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/bigger-is-greater/problem
+
+def swap(word, i, j):
+    temp = word[i]
+    word[i] = word[j]
+    word[j] = temp
+
+def next_bigger():
+    data = list(input())
+    k = 0 
+    for i in range(len(data)-1):
+        if data[i] < data[i+1]:
+            k = i
+
+    l = 0
+    for i in range(k+1, len(data)):
+        if data[i] > data[k]:
+            l = i
+
+    if k==l:
+        print('no answer')
+        return
+    
+    swap(data, k, l)
+    data = data[0:k+1] + data[len(data)-1:k:-1]
+    print(''.join(data))
+
+for i in range(int(input())):
+    next_bigger()
